b9da1075f518126d5f60a97b3a0a68e2d3f25e45
jonjeung <jon.jeung@gmail.com>
2018-11-09 15:10:29 -0600

    Added deck card lists for heroes, monsters, and environments

---
+++ app/src/main/java/rush/boss/edu/bossrush/deckcards/heroes.java
@@ -0,0 +1,18 @@
+package main.java.rush.boss.edu.bossrush.deckcards;
+
+import java.util.ArrayList;
+
+public class heroCards {
+    private ArrayList heroCards;
+
+    public ArrayList getHeroCards() {
+        return heroCards;
+    }
+
+        heroCards = new ArrayList<>();
+
+        heroCards.add("HER00001", "The Lost Boy", 100, 5, 5);
+        heroCards.add("HER00002", "The Weaponsmith", 100, 7, 3);
+        heroCards.add("HER00003", "The Medicine Man", 75, 3, 7);
+        heroCards.add("HER00004", "The Martial Artist", 125, 3, 4);
+}
