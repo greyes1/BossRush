8676ab0d1adcd05bfbfc3c32a35c9244550c6dd8
jonjeung <jon.jeung@gmail.com>
2018-11-10 13:48:54 -0600

    Added deck card lists for heroes, monsters, and environments

--- app/src/main/java/rush/boss/edu/bossrush/deckcards/heroCards.java
+++ app/src/main/java/rush/boss/edu/bossrush/deckcards/heroCards.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 
 public class heroCards {
 
-    ArrayList<Card> heroes = new ArrayList<>();
+    ArrayList<Card> heroes = new ArrayList<Card>();
 
     public ArrayList<Card> getCards() {
         return heroes;
     }
 
     Card hero1 = new Card("HER00001", "The Lost Boy", 100, 5, 5);
-    Card hero2 = new Card("HER00002", "The Weaponsmith", 100, 7, 3));
-    Card hero3 = new Card("HER00003", "The Medicine Man", 75, 3, 7));
-    Card hero4 = new Card("HER00004", "The Martial Artist", 125, 3, 4));
+    Card hero2 = new Card("HER00002", "The Weaponsmith", 100, 7, 3);
+    Card hero3 = new Card("HER00003", "The Medicine Man", 75, 3, 7);
+    Card hero4 = new Card("HER00004", "The Martial Artist", 125, 3, 4);
 
     heroes.add(hero1);
     heroes.add(hero2);
---
+++ app/src/main/java/rush/boss/edu/bossrush/deckcards/monsterCards.java
@@ -0,0 +1,20 @@
+package main.java.rush.boss.edu.bossrush.deckcards;
+
+import java.util.ArrayList;
+
+public class monsterCards {
+
+    ArrayList<Card> monsters = new ArrayList<>();
+
+//    public ArrayList<Card> getCards() {
+//        return monsters;
+//    }
+
+    Card monster1 = new Card("MON00001", "Violet Sorceress", 75, 11, 2);
+    Card monster2 = new Card("MON00002", "Last Defender", 125, 10, 3);
+    Card monster3 = new Card("MON00003", "Enraged Warrior", 100, 12, 1);
+
+    monsters.add(monster1);
+    monsters.add(monster2);
+    monsters.add(monster3);
+}
