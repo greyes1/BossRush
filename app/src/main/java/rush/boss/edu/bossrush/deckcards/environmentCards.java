272879440341fbe86245c1301b8c2a44ed511be1
jonjeung <jon.jeung@gmail.com>
2018-11-10 14:17:22 -0600

    Added deck card lists for heroes, monsters, and environments

--- app/src/main/java/rush/boss/edu/bossrush/deckcards/environmentCards.java
+++ app/src/main/java/rush/boss/edu/bossrush/deckcards/environmentCards.java
@@ -3,19 +3,24 @@ package main.java.rush.boss.edu.bossrush.deckcards;
 import java.util.ArrayList;
 
 public class environmentCards {
-    ArrayList<Card> environments = new ArrayList<>();
+
+    private static ArrayList<Card> environments = new ArrayList<>();
 
     public ArrayList<Card> getCards() {
         return environments;
     }
 
-    Card environment1 = new Card("ENV00001", "Desolate City", 0, 0, 0);
-    Card environment2 = new Card("ENV00002", "Ancient Temple", 0, 0, 0);
-    Card environment3 = new Card("ENV00003", "Volcanic Island", 0, 0, 0);
-    Card environment4 = new Card("ENV00004", "Sinking Ship", 0, 0, 0);
-    Card environment5 = new Card("ENV00005", "Cosmic Space", 0, 0, 0);
+    static {
+        Card environment1 = new Card("ENV00001", "Desolate City", 0, 0, 0);
+        Card environment2 = new Card("ENV00002", "Ancient Temple", 0, 0, 0);
+        Card environment3 = new Card("ENV00003", "Volcanic Island", 0, 0, 0);
+        Card environment4 = new Card("ENV00004", "Sinking Ship", 0, 0, 0);
+        Card environment5 = new Card("ENV00005", "Cosmic Space", 0, 0, 0);
 
-    environments.add(environment1);
-    environments.add(environment2);
-    environments.add(environment3);
+        environments.add(environment1);
+        environments.add(environment2);
+        environments.add(environment3);
+        environments.add(environment4);
+        environments.add(environment5);
+    }
 }
--- app/src/main/java/rush/boss/edu/bossrush/deckcards/heroCards.java
+++ app/src/main/java/rush/boss/edu/bossrush/deckcards/heroCards.java
@@ -4,19 +4,22 @@ import java.util.ArrayList;
 
 public class heroCards {
 
-    ArrayList<Card> heroes = new ArrayList<Card>();
+    private static ArrayList<Card> heroes = new ArrayList<>();
 
     public ArrayList<Card> getCards() {
         return heroes;
     }
 
-    Card hero1 = new Card("HER00001", "The Lost Boy", 100, 5, 5);
-    Card hero2 = new Card("HER00002", "The Weaponsmith", 100, 7, 3);
-    Card hero3 = new Card("HER00003", "The Medicine Man", 75, 3, 7);
-    Card hero4 = new Card("HER00004", "The Martial Artist", 125, 3, 4);
+    static {
+        Card hero1 = new Card("HER00001", "The Lost Boy", 100, 5, 5);
+        Card hero2 = new Card("HER00002", "The Weaponsmith", 100, 7, 3);
+        Card hero3 = new Card("HER00003", "The Medicine Man", 75, 3, 7);
+        Card hero4 = new Card("HER00004", "The Martial Artist", 125, 3, 4);
 
-    heroes.add(hero1);
-    heroes.add(hero2);
-    heroes.add(hero3);
-    heroes.add(hero4);
+
+        heroes.add(hero1);
+        heroes.add(hero2);
+        heroes.add(hero3);
+        heroes.add(hero4);
+    }
 }
